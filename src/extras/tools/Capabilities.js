7b6a75dc4fcf7979f95148b5894a948bffff6dcc
Stephens Nunnally <stevinz@gmail.com>
2023-01-20 14:37:02 -0800

    Improved capabilities

--- src/extras/tools/Capabilities.js
+++ src/extras/tools/Capabilities.js
@@ -1,8 +1,14 @@
+// https://www.khronos.org/registry/webgl/extensions/
+// https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/getParameter
+
 class Capabilities {
 
     constructor(renderer) {
         const gl = renderer.gl;
-        const extensions = renderer.extensions;
+
+        // Extensions
+        const anisotropicExt = renderer.getExtension('EXT_texture_filter_anisotropic');
+        const debugExt = renderer.getExtension('WEBGL_debug_renderer_info');
 
         // Shaders
         this.maxAttributes = gl.getParameter(gl.MAX_VERTEX_ATTRIBS);
@@ -25,12 +31,8 @@ class Capabilities {
         this.maxTextures = gl.getParameter(gl.MAX_COMBINED_TEXTURE_IMAGE_UNITS);
         this.maxTextureSize = gl.getParameter(gl.MAX_TEXTURE_SIZE);
         this.maxCubemapSize = gl.getParameter(gl.MAX_CUBE_MAP_TEXTURE_SIZE);
-
-        this.maxAnisotropy = 0;
-        if (extensions['EXT_texture_filter_anisotropic']) {
-            const extension = extensions['EXT_texture_filter_anisotropic'];
-			this.maxAnisotropy = gl.getParameter(extension.MAX_TEXTURE_MAX_ANISOTROPY_EXT);
-        }
+        this.maxArrayTextureLayers = gl.getParameter(gl.MAX_ARRAY_TEXTURE_LAYERS);
+        this.maxAnisotropy = (anisotropicExt) ? gl.getParameter(anisotropicExt.MAX_TEXTURE_MAX_ANISOTROPY_EXT) : 0;
 
         // Renderer
         this.maxSamples = gl.getParameter(gl.MAX_SAMPLES);
@@ -45,52 +47,71 @@ class Capabilities {
 
         // Console Logger
         this.toHtml = function() {
-            let out = `
-            <b>Shaders</b> <br />
-            &emsp;Max Shader Precision: ${this.maxPrecision} <br />
-            &emsp;Max Vertex Attributes: ${this.maxAttributes} <br />
-            &emsp;Max Varying Vectors: ${this.maxVaryings} <br />
-            &emsp;Max Vertex Uniform Vectors: ${this.maxVertexUniforms} <br />
-            &emsp;Max Fragment Uniform Vectors: ${this.maxFragmentUniforms} <br />
-            <b>Textures</b> <br />
-            &emsp;Max Fragment Textures: ${this.maxFragmentTextures} <br />
-            &emsp;Max Vertex Textures: ${this.maxVertexTextures} <br />
-            &emsp;Max Combined Textures: ${this.maxTextures} <br />
-            &emsp;Max 2D Texture Size: ${this.maxTextureSize} <br />
-            &emsp;Max Cube Texture Size: ${this.maxCubemapSize} <br />
-            &emsp;Max Texture Anisotropy: ${this.maxAnisotropy} <br />
-            <b>Renderer</b> <br />
-            &emsp;Point Size Range: ${gl.getParameter(gl.ALIASED_POINT_SIZE_RANGE)[0]} - ${gl.getParameter(gl.ALIASED_POINT_SIZE_RANGE)[1]} <br />
-            &emsp;Line Width Range: ${gl.getParameter(gl.ALIASED_LINE_WIDTH_RANGE)[0]} - ${gl.getParameter(gl.ALIASED_LINE_WIDTH_RANGE)[1]} <br />
-            &emsp;Max Viewport Dimensions: ${gl.getParameter(gl.MAX_VIEWPORT_DIMS)[0]} - ${gl.getParameter(gl.MAX_VIEWPORT_DIMS)[1]} <br />
-            &emsp;Max Renderbuffer Size: ${gl.getParameter(gl.MAX_RENDERBUFFER_SIZE)} <br />
-            &emsp;Max Fragment Shader Multiple Render Targets: ${this.drawBuffers} <br />
-            &emsp;Max MSAA Samples: ${this.maxSamples} <br />
-            <b>Framebuffer</b> <br />
-            &emsp;Framebuffer Red Bits: ${gl.getParameter(gl.RED_BITS)} <br />
-            &emsp;Framebuffer Green Bits: ${gl.getParameter(gl.GREEN_BITS)} <br />
-            &emsp;Framebuffer Blue Bits: ${gl.getParameter(gl.BLUE_BITS)} <br />
-            &emsp;Framebuffer Alpha Bits: ${gl.getParameter(gl.ALPHA_BITS)} <br />
-            &emsp;Framebuffer Depth Bits: ${gl.getParameter(gl.DEPTH_BITS)} <br />
-            &emsp;Framebuffer Stencil Bits: ${gl.getParameter(gl.STENCIL_BITS)} <br />
-            &emsp;Framebuffer Subpixel Bits: ${gl.getParameter(gl.SUBPIXEL_BITS)} <br />
-            <b>Framebuffer Types</b> <br />
-            &emsp;Support for Unsigned Byte Render Targets: ${this.byteTargets} <br />
-            &emsp;Support for Float Render Targets: ${this.floatTargets} <br />
-            &emsp;Support for Half Float Render Targets: ${this.halfFloatTargets} <br />
-            <b>Supported Extensions</b> <br />
-            &emsp;${gl.getSupportedExtensions().join('<br />&emsp;')} <br />
-            <b>WebGL</b> <br />
-            &emsp;WebGL Renderer: ${gl.getParameter(gl.RENDERER)} <br />
-            &emsp;WebGL Vendor: ${gl.getParameter(gl.VENDOR)} <br />
-            &emsp;WebGL Version: ${gl.getParameter(gl.VERSION)} <br />
-            &emsp;Shading Language: ${gl.getParameter(gl.SHADING_LANGUAGE_VERSION)} <br />
+            const debugRenderer = (debugExt) ? `Unmasked Renderer: <span class="Light">${gl.getParameter(debugExt.UNMASKED_RENDERER_WEBGL)}</span> <br />` : ``;
+            const debugVendor = (debugExt) ? `Unmasked Vendor: <span class="Light">${gl.getParameter(debugExt.UNMASKED_VENDOR_WEBGL)}</span> <br />` : ``;
+            return `
+                <details open="true">
+                <summary>Browser</summary>
+                Platform: <span class="Light">${(navigator.userAgentData) ? navigator.userAgentData.platform : navigator.platform}</span> <br />
+                User Agent: <span class="Light">${navigator.userAgent}</span> <br />
+                </details>
+                <details open="true">
+                <summary>WebGL</summary>
+                WebGL Version: <span class="Light">${gl.getParameter(gl.VERSION)}</span> <br />
+                WebGL Vendor: <span class="Light">${gl.getParameter(gl.VENDOR)}</span> <br />
+                WebGL Renderer: <span class="Light">${gl.getParameter(gl.RENDERER)}</span> <br />
+                Shading Language: <span class="Light">${gl.getParameter(gl.SHADING_LANGUAGE_VERSION)}</span> <br />
+                ${debugRenderer}
+                ${debugVendor}
+                </details>
+                <details open="true">
+                <summary>Shaders</summary>
+                Max Shader Precision: <span class="Light">${this.maxPrecision}</span> <br />
+                Max Vertex Attributes: <span class="Light">${this.maxAttributes}</span> <br />
+                Max Varying Vectors: <span class="Light">${this.maxVaryings}</span> <br />
+                Max Vertex Uniform Vectors: <span class="Light">${this.maxVertexUniforms}</span> <br />
+                Max Fragment Uniform Vectors: <span class="Light">${this.maxFragmentUniforms}</span> <br />
+                </details>
+                <details open="true">
+                <summary>Textures</summary>
+                Max Fragment Textures: <span class="Light">${this.maxFragmentTextures}</span> <br />
+                Max Vertex Textures: <span class="Light">${this.maxVertexTextures}</span> <br />
+                Max Combined Textures: <span class="Light">${this.maxTextures}</span> <br />
+                Max 2D Texture Size: <span class="Light">${this.maxTextureSize}</span> <br />
+                Max Cube Texture Size: <span class="Light">${this.maxCubemapSize}</span> <br />
+                Max Array Texture Layers: <span class="Light">${this.maxArrayTextureLayers}</span> <br />
+                Max Texture Anisotropy: <span class="Light">${this.maxAnisotropy}</span> <br />
+                </details>
+                <details open="true">
+                <summary>Renderer</summary>
+                Point Size Range: <span class="Light">${gl.getParameter(gl.ALIASED_POINT_SIZE_RANGE)[0]} - ${gl.getParameter(gl.ALIASED_POINT_SIZE_RANGE)[1]}</span> <br />
+                Line Width Range: <span class="Light">${gl.getParameter(gl.ALIASED_LINE_WIDTH_RANGE)[0]} - ${gl.getParameter(gl.ALIASED_LINE_WIDTH_RANGE)[1]}</span> <br />
+                Max Viewport Dimensions: <span class="Light">${gl.getParameter(gl.MAX_VIEWPORT_DIMS)[0]} - ${gl.getParameter(gl.MAX_VIEWPORT_DIMS)[1]}</span> <br />
+                Max Renderbuffer Size: <span class="Light">${gl.getParameter(gl.MAX_RENDERBUFFER_SIZE)}</span> <br />
+                Max Fragment Shader Multiple Render Targets: <span class="Light">${this.drawBuffers}</span> <br />
+                Max MSAA Samples: <span class="Light">${this.maxSamples}</span> <br />
+                </details>
+                <details open="true">
+                <summary>Framebuffer</summary>
+                Framebuffer Red Bits: <span class="Light">${gl.getParameter(gl.RED_BITS)}</span> <br />
+                Framebuffer Green Bits: <span class="Light">${gl.getParameter(gl.GREEN_BITS)}</span> <br />
+                Framebuffer Blue Bits: <span class="Light">${gl.getParameter(gl.BLUE_BITS)}</span> <br />
+                Framebuffer Alpha Bits: <span class="Light">${gl.getParameter(gl.ALPHA_BITS)}</span> <br />
+                Framebuffer Depth Bits: <span class="Light">${gl.getParameter(gl.DEPTH_BITS)}</span> <br />
+                Framebuffer Stencil Bits: <span class="Light">${gl.getParameter(gl.STENCIL_BITS)}</span> <br />
+                Framebuffer Subpixel Bits: <span class="Light">${gl.getParameter(gl.SUBPIXEL_BITS)}</span> <br />
+                </details>
+                <details open="true">
+                <summary>Framebuffer Types</summary>
+                Support for Unsigned Byte Render Targets: <span class="Light">${this.byteTargets}</span> <br />
+                Support for Float Render Targets: <span class="Light">${this.floatTargets}</span> <br />
+                Support for Half Float Render Targets: <span class="Light">${this.halfFloatTargets}</span> <br />
+                </details>
+                <details open="true">
+                <summary>Supported Extensions</summary>
+                ${gl.getSupportedExtensions().join('<br />')} <br />
+                </details>
             `;
-            if (extensions['WEBGL_debug_renderer_info']) {
-                out += `&emsp;Unmasked Renderer: ${gl.getParameter(extensions['WEBGL_debug_renderer_info'].UNMASKED_RENDERER_WEBGL)} <br />`;
-                out += `&emsp;Unmasked Vendor: ${gl.getParameter(extensions['WEBGL_debug_renderer_info'].UNMASKED_VENDOR_WEBGL)} <br />`;
-            }
-            return out;
         }
     }
 
