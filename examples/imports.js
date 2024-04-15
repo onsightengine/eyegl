a7f1ea6b9f221d72ddea799047715ec122dd8657
Stephens Nunnally <stevinz@gmail.com>
2024-04-15 10:44:20 -0700

    Lock suey version

--- examples/imports.js
+++ examples/imports.js
@@ -1,5 +1,10 @@
 /***** Local *****/
 
+// "seagl": "../../src/SeaGL.js",
+// "suey": "../../../suey/build/suey.module.js"
+
+/***** CDN *****/
+
 // "seagl": "https://unpkg.com/seagl/build/seagl.module.js"
 // "suey": "https://unpkg.com/suey/build/suey.module.js"
 
@@ -8,7 +13,7 @@ document.write(`
     {
         "imports": {
             "seagl": "../../src/SeaGL.js",
-            "suey": "../../../suey/build/suey.module.js"
+            "suey": "https://unpkg.com/suey@0.1.24/build/suey.module.js"
         }
     }
     </script>
