ce64de30e619b9062d039cc0bb6ef2052f50b271
Stephens Nunnally <stevinz@gmail.com>
2024-07-24 12:21:52 -0700

    Update gui lib

--- examples/imports.js
+++ examples/imports.js
@@ -6,14 +6,14 @@
 /***** CDN *****/
 
 // "eyegl": "https://unpkg.com/eyegl/dist/eyegl.module.js"
-// "suey": "https://unpkg.com/@salinity/suey/dist/suey.module.js"
+// "suey": "https://unpkg.com/suey/dist/suey.module.js"
 
 document.write(`
     <script type='importmap'>
     {
         "imports": {
             "eyegl": "../../src/EyeGL.js",
-            "suey": "https://unpkg.com/@salinity/suey@0.1.30/dist/suey.module.js"
+            "suey": "https://unpkg.com/suey@0.1.32/dist/suey.module.js"
         }
     }
     </script>
